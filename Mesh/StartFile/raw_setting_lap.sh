562056e1abe94312dd14e418de453d4203f00805
Junho Kim <junhoya924@hanmail.net>
2016-11-08 17:16:32 +0900

    raw_setting pi & lap
    raw_setting pi & lap

--- Mesh/StartFile/raw_setting_lap.sh
+++ Mesh/StartFile/raw_setting_lap.sh
@@ -9,17 +9,13 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java9-installer -y
 sudo apt-get install openjdk-8-jdk tomcat8 -y
-sudo apt-get install libglib2.0-dev libudev-dev libical-dev libreadline-dev bluez-tools -y
-
-sudo wget http://launchpadlibrarian.net/214461570/bluez-hcidump_5.33-0ubuntu2_armhf.deb
-sudo mv bluez-hcidump_5.33-0ubuntu2_armhf.deb ~/Downloads/bluez-hcidump_5.33-0ubuntu2_armhf.deb
-sudo dpkg -i ~/Downloads/bluez-hcidump_5.33-0ubuntu2_armhf.deb
+sudo apt-get install libglib2.0-dev libudev-dev libical-dev libreadline-dev bluez-tools hcidump -y
 
 sudo git clone https://github.com/OpenWinCon/OpenWinNet.git ~/Downloads/OpenWinNet
 sudo cp ~/Downloads/OpenWinNet/Mesh/StartFile/masterWebServer.war /var/lib/tomcat8/webapps
 
 sudo service tomcat8 restart
 
-sudo add-apt-repository ppa:abbat/tox
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/antonbatenev:/tox/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/qtox.list"
 sudo apt-get update
 sudo apt-get install qtox -y
