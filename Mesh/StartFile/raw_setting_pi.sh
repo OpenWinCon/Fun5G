f3bfa928649acdcd77ead35ddab477818bfafb77
Junho Kim <junhoya924@hanmail.net>
2016-11-08 16:08:29 +0900

    raw_setting_pi
    first install programs(batctl, dhcp, hostapd, gotham, etc.)

---
+++ Mesh/StartFile/raw_setting_pi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ssh git batctl -y
+sudo apt-get install iw hostapd isc-dhcp-server -y
+sudo apt-get install openvswitch-switch -y
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java9-installer -y
+sudo apt-get install openjdk-8-jdk tomcat8 -y
+sudo apt-get install libglib2.0-dev libudev-dev libical-dev libreadline-dev bluez-tools -y
+
+sudo wget http://launchpadlibrarian.net/214461570/bluez-hcidump_5.33-0ubuntu2_armhf.deb
+sudo mv bluez-hcidump_5.33-0ubuntu2_armhf.deb ~/Downloads/bluez-hcidump_5.33-0ubuntu2_armhf.deb
+sudo dpkg -i ~/Downloads/bluez-hcidump_5.33-0ubuntu2_armhf.deb
+
+sudo git clone https://github.com/OpenWinCon/OpenWinNet.git ~/Downloads/OpenWinNet
+sudo cp ~/Downloads/OpenWinNet/Mesh/StartFile/masterWebServer.war /var/lib/tomcat8/webapps
+
+sudo service tomcat8 restart
