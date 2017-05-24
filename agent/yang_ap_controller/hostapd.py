15ac6f753e4dc45ab3cf51045647dff5e77681c8
안계완 <angyewan@angyewan-ui-Mac-Pro.local>
2017-05-24 14:19:28 +0900

    add yang based ap server and controller

---
+++ agent/yang_ap_controller/hostapd.py
@@ -0,0 +1,225 @@
+"""
+
+주석 부분 입력할거임.
+
+
+"""
+
+import os
+import platform
+
+'''
+class ApWrapper:
+    def __init__(self):
+        self.hostapd = ''
+        self.context = device_config_pb2.Devices.AccessPoint.Ap()
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            return None
+        return self.context.access_point_information.ap_list[key]
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, int):
+            return
+        if not isinstance(value, tuple):
+            return
+
+        ap = self.context.access_point_information.ap_list[key]
+        ap[value[0]] = value[1]
+
+    def append(self, item):
+        self.context.access_point_information.ap_list.extend([item])
+'''
+
+
+class hostapd:
+
+    def get_ip_address(self, ifname):
+        if self.plat == 'Linux':
+            ip = os.popen('ip addr show ' + ifname).read().split("inet ")[1].split("/")[0]
+        else:
+            ip = '127.0.0.1'
+        return ip
+
+    def __init__(self, ap_listener, path = "./openwinnet.conf"):
+
+        self.path = path
+        self.plat = platform.system()
+        self.ap_listener = ap_listener
+
+        self.is_starting = False
+        self.version = -1
+
+        ### mapping method
+        self.method_map = {
+            'ssid' : 'set_ssid',
+            'ss' : 'set_ssid',
+            'name' : 'set_ssid',
+            'password' : 'set_password',
+            'pwd' : 'set_password',
+            'channel' : 'set_channel'
+        }
+
+        ### default configurable dictionary
+        self.config_dict = {
+            'ssid': 'pqoqoqoqmfodqmfo',
+            'ip': '127.0.0.1',
+            'channel' : '1',
+            'hw_mode' : 'g',
+            'power_on_off' : '0'
+        }
+
+
+        ### default static dictionary
+        self.static_dict = {
+            'wpa_passphrase': '12345678',
+            'interface' : 'wlan0',
+            'wpa' : '3',
+            'wpa_key_mgmt' : 'WPA-PSK',
+            'wpa_pairwise' : 'TKIP',
+            'rsn_pairwise' : 'CCMP',
+            'auth_algs' : '1',
+            'driver' : 'nl80211',
+            'ctrl_interface' : '/var/run/hostapd',
+            'ctrl_interface_group' : '0',
+            'ieee80211n' : '1',
+            'wmm_enabled' : '1',
+            'ht_capab' : '[HT20][SHORT-GI[20]'
+        }
+
+
+        self.read_config()
+        self.config_dict['ip'] = self.get_ip_address('eth0')
+
+        self.ap_listener_init()
+
+
+
+    def ap_listener_init(self):
+        self.ap_listener.on_ap_changed(
+            self.config_dict
+        )
+
+
+    def _edit_config(self, command, value):
+        import inspect
+
+        if command in self.method_map:
+            command = self.method_map[command]
+
+        if hasattr(self, command):
+            func = getattr(self, command)
+            if callable(func):
+                if len(inspect.getargspec(func).args) > 1:
+                    func(value)
+                    self.write_config()
+                    if self.is_starting == True:
+                        self.stop()
+                        self.start()
+                else:
+                    func()
+
+
+
+    #### setter
+    def set_ssid(self, value):
+        self.config_dict['ssid'] = value
+        self.ap_listener.on_ap_changed(
+            {'ssid': value }
+        )
+
+    def set_channel(self, value):
+        self.config_dict['channel'] = value
+        self.ap_listener.on_ap_changed(
+            { 'channel' : value }
+        )
+
+    def set_mode(self, value):
+        self.config_dict['hw_mode'] = value
+        self.ap_listener.on_ap_changed(
+            { 'hw_mode' : value }
+        )
+
+    def set_power_on_off(self, value):
+        self.config_dict['power_on_off'] = value
+        self.ap_listener.on_ap_changed(
+            { 'power_on_off' : value }
+        )
+
+
+    def read_config(self):
+        f = open('./openwinnet.conf', 'r')
+        lines = f.readlines()
+        for line in lines:
+            key, val = line.split('=')
+            val = val.replace('\n', '')
+            if key in self.static_dict:
+                if val != '' or len(val) != 0 or val is not None:
+                    self.static_dict[key] = val
+            elif key in self.config_dict:
+                self.config_dict[key] = val
+        f.close()
+
+
+    def write_config(self):
+        f = open('./openwinnet.conf', 'w')
+
+        for key, val in self.config_dict.items():
+            if key != 'ip' and key != 'power_on_off':
+                f.write(key + '=' + val + '\n')
+        for key, val in self.static_dict.items():
+            f.write(key + '=' + val + '\n')
+        f.close()
+
+
+
+    def start(self):
+        import subprocess
+
+        if self.plat == 'Linux':
+            print(subprocess.getoutput("nmcli radio wifi off"))
+            print(subprocess.getoutput("rfkill unblock wlan"))
+            print(subprocess.getoutput("ifconfig wlan0 192.168.1.34 up"))
+            print(subprocess.getoutput("dhcpd"))
+            print(subprocess.getoutput("hostapd -dd " + self.path + " -B"))
+
+            '''
+            subprocess.run(['nmcli', 'radio', 'wifi', 'off'])
+            subprocess.run(['rfkill', 'unblock', 'wlan'])
+            subprocess.run(['ifconfig', 'wlan0', '192.163.1.34', 'up'])
+            subprocess.run(['dhcpd'])
+            subprocess.run(['hostapd', '-dd', self.path, ' -B'])
+            '''
+            self.is_starting = True
+
+        else:
+            print ("We will develop other operating systems.")
+
+    def stop(self):
+        import subprocess
+        if self.plat == 'Linux':
+            '''
+            subprocess.run(['skill', 'dhcpd'])
+            subprocess.run(['skill', 'hostapd'])
+            '''
+            subprocess.getoutput("skill dhcpd")
+            subprocess.getoutput("skill hostapd")
+            self.is_starting = False
+
+        else:
+            print("We will develop other operating systems.")
+
+  #  def stop_hostapd(self):
+  #
+
+
+
+
+
+
+
+
+
+
+
---
+++ agent/yang_ap_controller/yang_based_ap_controller_by_grpc.py
@@ -0,0 +1,182 @@
+# Copyright 2015, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# next = dv.ap_list.add()
+# next.ssid="sss"
+"""
+  #print(len(dv.ApList))
+  print(dv.ap_list[0].ssid)
+  print(dv.ap_list[1].ssid)
+
+  new_AP = device_config_pb2.Devices.NetworkDevices.AccessPointInformation.ApList()
+  new_AP.ssid = "new"
+
+  dv.ap_list.extend([new_AP])
+
+ # print (len(dv.ApList))
+  print (dv.ApList[0].ssid)
+  print (dv.ApList[1].ssid)
+  print(dv.ApList[2].ssid)
+"""
+# apd = hostapd(dv)
+# print(dv.ApList.ssid)
+# print(apd.ssid)
+# dv = device_config_pb2.Devices()
+# dev = hostapd(dv)
+
+# print (dev.ip)
+# print (dev.ap.ssid)
+# print (dev)
+#
+# print (dev.ap)
+from __future__ import print_function
+
+import grpc
+
+import device_config_pb2
+import device_config_pb2_grpc
+
+from hostapd import hostapd
+
+
+def run():
+    while(1):
+        command = input("wRFCONF> ")
+        cmd_list = command.split()
+        ip = cmd_list[0]
+        cmd = cmd_list[1]
+        length = len(cmd_list)
+        channel = grpc.insecure_channel(ip + ':50051')
+        stub = device_config_pb2_grpc.DeviceConfigStub(channel)
+        if cmd == 'hello':
+            reponse = stub.Hello(device_config_pb2.HelloRequest(hello='Hello'))
+            print("Power: Off")
+            print("IP: %s" % reponse.ip)
+            print("SSID: %s" % reponse.ssid)
+            print("channel: %s" % reponse.channel)
+            print("mode: %s" % reponse.hw_mode)
+        elif cmd == 'edit' or cmd == 'edit_config':
+            if cmd_list[2] is None or cmd_list[3] is None:
+                print("please enter value")
+            else:
+                reponse = stub.EditConfig(device_config_pb2.EditConfigRequest(command=cmd_list[2], value=cmd_list[3]))
+                print("Power: Off")
+                print("IP: %s" % reponse.ip)
+                print("SSID: %s" % reponse.ssid)
+                print("channel: %s" % reponse.channel)
+                print("mode: %s" % reponse.hw_mode)
+
+
+
+
+
+
+'''
+    channel = grpc.insecure_channel('163.180.118.62:50051')
+    stub = device_config_pb2_grpc.DeviceConfigStub(channel)
+
+    print("---------------------------------------------------")
+    print("Initial Session")
+    print("---------------------------------------------------")
+    print("Send Hello Request")
+    print("---------------------------------------------------")
+    reponse = stub.Hello(device_config_pb2.HelloRequest(hello='Hello'))
+    print("Power: Off")
+    print("IP: %s" % reponse.ip)
+    print("SSID: %s" % reponse.ssid)
+    print("password: openwincon_1")
+    print("channel: %s" % reponse.channel)
+    print("mode: %s" % reponse.hw_mode)
+    print("broadcasting: on")
+    print("---------------------------------------------------")
+
+    reponse = stub.EditConfig(device_config_pb2.EditConfigRequest(command='start'))
+    print("Send Edit Config Request(Start command)")
+    print("---------------------------------------------------")
+    print("Power: On")
+    print("IP: 163.180.118.62")
+    print("SSID: %s" % reponse.ssid)
+    print("password: openwincon_1")
+    print("channel: %s" % reponse.channel)
+    print("mode: %s" % reponse.hw_mode)
+    print("broadcasting: on")
+    print("---------------------------------------------------")
+
+    reponse = stub.EditConfig(device_config_pb2.EditConfigRequest(command='ssid', value='openwincon_test'))
+    reponse = stub.EditConfig(device_config_pb2.EditConfigRequest(command='channel', value='8'))
+
+    print("Send Edit Config Request(edit configuration command)")
+    print("---------------------------------------------------")
+    print("Power: On")
+    print("IP: 163.180.118.62")
+    print("SSID: %s" % reponse.ssid)
+    print("password: openwincon_1")
+    print("channel: %s" % reponse.channel)
+    print("mode: %s" % reponse.hw_mode)
+    print("broadcasting: on")
+    print("---------------------------------------------------")
+'''
+
+# print(response)
+
+def _edit_config(stub, in_command, in_value):
+    print("Send Edit Config Request " + in_command)
+    response = stub.EditConfig(device_config_pb2.EditConfigRequest(command=in_command, value=in_value))
+    print("---------------------------------------------------")
+    print("Power: On")
+    print("IP: %s" % response.ip)
+    print("SSID: %s" % response.ssid)
+    print("password: openwincon_1")
+    print("channel: %s" % response.channel)
+    print("mode: %s" % response.hw_mode)
+    print("broadcasting: on")
+
+
+# response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+# print("Greeter client received: " + response.result)
+
+def _hello(ip, port):
+    channel = grpc.insecure_channel(ip + ':' + port)
+    stub = device_config_pb2_grpc.DeviceConfigStub(channel)
+
+    response = stub.Hello(device_config_pb2.HelloRequest(hello='Hello'))
+    print("Power: Off")
+    print("IP: %s" % response.ip)
+    print("SSID: %s" % response.ssid)
+    print("password: openwincon_1")
+    print("channel: %s" % response.channel)
+    print("mode: %s" % response.hw_mode)
+    print("broadcasting: on")
+    print("---------------------------------------------------")
+
+    return stub
+
+
+if __name__ == '__main__':
+    run()
